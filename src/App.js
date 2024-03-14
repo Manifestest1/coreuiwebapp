580f9cbb5cf78b31cc75e55c58b09f17194294a9
Manifestest1 <manifestest1@gmail.com>
2024-03-14 20:10:09 +0530

    'changes'

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect,useState } from 'react';
+import AppHeader from './views/layout/AppHeader';
+import AppFooter from './views/layout/AppFooter';
+import Home from './views/components/Home';
 
-function App() {
+function App() 
+{
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+     <AppHeader/>
+       <Home/>
+      <AppFooter/>
+  </>
   );
 }
 
